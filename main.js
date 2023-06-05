95bb5ca6cfcf5b5d9cac75f42613f9c04a1222ea
jsilvaco <josef0330@hotmail.com>
2023-06-05 17:00:23 -0500

    Maqueteado y funcionalidad de la lista de productos

--- main.js
+++ main.js
@@ -1,8 +1,112 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail')
+const mobileMenu = document.querySelector('.mobile-menu');
+const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
+menuHamIcon.addEventListener('click',toggleMobileMenu);
+menuCarritoIcon.addEventListener('click',toggleCarritoMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu() {
+    const isAsideClose = aside.classList.contains('inactive');
+
+    if(!isAsideClose){
+        aside.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoMenu() {
+    const isMobileMenuClose = mobileMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClose){
+        mobileMenu.classList.add('inactive');
+    }
+    
+    aside.classList.toggle('inactive');
+    
+}
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+productList.push({
+    name: 'Pantalla',
+    price: 500,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+productList.push({
+    name: 'Pantalon',
+    price: 300,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+productList.push({
+    name: 'CPU',
+    price: 400,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+
+function renderProducts(arr){
+    for(product of productList){
+    
+        /*Inserta productinfo y la Img*/
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+        
+        
+        /*Insertar dentro del div Precio y nombre y el figure*/
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        /*insertar precio y nombre*/
+        const productInfoDiv = document.createElement('div');
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+        
+        /*Inserta la Fifura*/
+        const productInfoFigure = document.createElement('figure');
+        const productImgCard = document.createElement('img');
+        productImgCard.setAttribute('src', './icons/bt_add_to_cart.svg');
+        
+        /*Inserta la Fifura*/
+        productInfoFigure.appendChild(productImgCard);
+
+        /*insertar precio y nombre*/
+        productInfoDiv.append(productPrice, productName);
+        //productInfoDiv.appendChild(productName);
+        
+        /*Insertar dentro del div Precio y nombre y el figure*/
+        productInfo.append(productInfoDiv, productInfoFigure);
+        //productInfo.appendChild(productInfoFigure);
+
+        /*Inserta productinfo y la Img*/
+        productCard.append(productImg, productInfo);
+        //productCard.appendChild(productInfo);
+
+        /*Insertación de todo el maquetado en el contenedor */
+        cardsContainer.appendChild(productCard);
+
+    }
+}
+
+renderProducts(productList);
